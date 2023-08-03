a67da1a5d90bcaf5e92c9cfe795e6bf9528cdf5d
Darshan Chobarkar <dchobarkar@gmail.com>
2023-08-03 21:45:09 +0530

    update pages

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,23 +1,22 @@
-import React, { useState, useEffect } from 'react'
-import styled from 'styled-components'
-// Components
-import PageLink from 'components/PageLink'
-// Content
-import messages from '../content/FourOhFour.yaml'
-// Styles
-import { glitchAnimation, glitchHoverAnimation } from 'styles/animations'
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { glitchAnimation, glitchHoverAnimation } from 'styles/animations';
 
+import PageLink from 'components/PageLink';
+import messages from '../content/FourOhFour.yaml';
 
 const Glitch = styled.div`
   ${glitchAnimation('2s')};
   left: -2px;
-  text-shadow: 2px 0 #F9F8F8, -1px 0 cyan, -2px 0 green, 3px 0 rgba(255,0,0,0.7);
-`
+  text-shadow: 2px 0 #f9f8f8, -1px 0 cyan, -2px 0 green,
+    3px 0 rgba(255, 0, 0, 0.7);
+`;
 
 const Title = styled.h1`
   font-size: 13.25rem;
   &:hover {
-    &:before, &:after {
+    &:before,
+    &:after {
       content: '404';
       position: absolute;
       text-align: center;
@@ -28,21 +27,22 @@ const Title = styled.h1`
     }
     &:before {
       ${glitchHoverAnimation('1s', '250ms', '')};
-      color: #0FF;
+      color: #0ff;
       z-index: -2;
     }
     &:after {
       ${glitchHoverAnimation('600ms', '0ms', 'reverse')};
-      color: #F05;
+      color: #f05;
       z-index: -1;
     }
   }
-`
+`;
 
 const Button = styled(PageLink)`
   ${glitchAnimation('2s')};
   transform-style: preserve-3d;
-  &::before, &::after {
+  &::before,
+  &::after {
     position: absolute;
     width: 102%;
     height: 70px;
@@ -57,64 +57,72 @@ const Button = styled(PageLink)`
     &:before {
       ${glitchHoverAnimation('1s', '250ms', 'reverse')};
       content: '';
-      background: #0FFA;
-      border-color: #0FF;
+      background: #0ffa;
+      border-color: #0ff;
       z-index: -2;
       transform: translateZ(-2px);
     }
     &:after {
       ${glitchHoverAnimation('600ms', '0ms', '')};
       content: '';
-      background: #F05A;
-      border-color: #F05;
+      background: #f05a;
+      border-color: #f05;
       z-index: -1;
       transform: translateZ(-1px);
     }
   }
-`
+`;
 
 const FourOhFour = () => {
-
-  const [message, setMessage] = useState({ text: '', buttonText: '' })
+  const [message, setMessage] = useState({ text: '', buttonText: '' });
 
   useEffect(() => {
-    let randomIndex = Math.floor(Math.random() * messages.length)
-    setMessage(messages[randomIndex])
-  }, [])
+    let randomIndex = Math.floor(Math.random() * messages.length);
+    setMessage(messages[randomIndex]);
+  }, []);
 
   return (
-    <div className='absolute bg-white font-title w-full h-full top-0 left-0 -translate-y-1/2 overflow-hidden' role='main'>
+    <div
+      className="absolute bg-white font-title w-full h-full top-0 left-0 -translate-y-1/2 overflow-hidden"
+      role="main"
+    >
       <div>
-        <Title className='fourohfour-title relative font-title font-extrabold text-base-300 leading-none text-center uppercase mt-18 mb-10 z-10 cursor-default'>
-          <span className='z-10'>404</span>
+        <Title className="fourohfour-title relative font-title font-extrabold text-base-300 leading-none text-center uppercase mt-18 mb-10 z-10 cursor-default">
+          <span className="z-10">404</span>
         </Title>
-        <div className='text-xl font-normal text-center m-auto mt-4'>
+
+        <div className="text-xl font-normal text-center m-auto mt-4">
           {message.text}
         </div>
-        <Glitch className='glitch absolute w-full top-0 overflow-hidden'>
-          <Title className='relative font-title font-extrabold text-base-300 leading-none text-center uppercase mt-18 mb-10 cursor-default'>404</Title>
+
+        <Glitch className="glitch absolute w-full top-0 overflow-hidden">
+          <Title className="relative font-title font-extrabold text-base-300 leading-none text-center uppercase mt-18 mb-10 cursor-default">
+            404
+          </Title>
         </Glitch>
       </div>
-      <div className='relative flex w-auto h-auto items-center mx-auto mt-28 mb-0 z-10'>
+
+      <div className="relative flex w-auto h-auto items-center mx-auto mt-28 mb-0 z-10">
         <Button
-          className='fourohfour-button group relative block bg-white text-base-400 text-lg font-bold text-center w-max min-w-48 py-4 px-6 mx-auto transition duration-300 ease outline-none cursor-pointer hover:text-base-300'
-          data-text='Home'
-          label='Home'
-          to='/'
+          className="fourohfour-button group relative block bg-white text-base-400 text-lg font-bold text-center w-max min-w-48 py-4 px-6 mx-auto transition duration-300 ease outline-none cursor-pointer hover:text-base-300"
+          data-text="Home"
+          label="Home"
+          to="/"
         >
           {message.buttonText.length ? message.buttonText : 'Home'}
+
           <PageLink
-            className='fourohfour-button group absolute block bg-white text-base-400 text-lg font-bold text-center w-max min-w-48 top-0 left-0 border-4 border-solid border-base-400 py-4 px-6 mx-auto transition duration-300 ease outline-none cursor-pointer hover:text-base-300'
-            data-text='Home'
-            label='Home'
-            to='/'
+            className="fourohfour-button group absolute block bg-white text-base-400 text-lg font-bold text-center w-max min-w-48 top-0 left-0 border-4 border-solid border-base-400 py-4 px-6 mx-auto transition duration-300 ease outline-none cursor-pointer hover:text-base-300"
+            data-text="Home"
+            label="Home"
+            to="/"
           >
             {message.buttonText.length ? message.buttonText : 'Home'}
           </PageLink>
         </Button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FourOhFour
+export default FourOhFour;
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,56 +1,57 @@
-import React from 'react'
-// Components
-import Layout from 'components/Layout'
-import Header from 'components/Header'
-import { Form, Input, TextArea, Button } from 'components/Forms'
-// Elements
-import Content from 'elements/Content'
-import Inner from 'elements/Inner'
-import { BigTitle } from 'elements/Titles'
-// Styles
-import 'styles/contact.css'
-// Hooks
-import useWindowSize from 'hooks/useWindowSize'
+import React from 'react';
 
+import Layout from 'components/Layout';
+import Header from 'components/Header';
+import { Form, Input, TextArea, Button } from 'components/Forms';
+import Content from 'elements/Content';
+import Inner from 'elements/Inner';
+import { BigTitle } from 'elements/Titles';
+import useWindowSize from 'hooks/useWindowSize';
+import 'styles/contact.css';
 
 const ContactPage = () => {
-  const windowSize = useWindowSize()
+  const windowSize = useWindowSize();
   return (
     <Layout windowSize={windowSize}>
       <Header>
-        <BigTitle className='md:mt-32 mt-18'>
-          Say<br />
-          Hello<span className='text-accent accent-dot'>.</span>
+        <BigTitle className="md:mt-32 mt-18">
+          Say
+          <br />
+          Hello<span className="text-accent accent-dot">.</span>
         </BigTitle>
       </Header>
-      <Content className='-mt-8 pb-32 md:mt-0 md:pb-28 lg:pt-6' role='main'>
-        <Inner className='my-8 sm:mt-8 mt-0'>
+
+      <Content className="-mt-8 pb-32 md:mt-0 md:pb-28 lg:pt-6" role="main">
+        <Inner className="my-8 sm:mt-8 mt-0">
           <ContactForm />
         </Inner>
       </Content>
     </Layout>
-  )
-}
+  );
+};
 
 class ContactForm extends React.Component {
-
   state = {
     name: '',
     email: '',
     subject: '',
     message: '',
-  }
+  };
 
   handleInputChange = (evt) => {
-    const target = evt.target
+    const target = evt.target;
     this.setState({
       [target.name]: target.value,
-    })
-  }
+    });
+  };
 
   render() {
     return (
-      <Form className='mt-8 w-full lg:w-9/10 xl:w-5/6' name={'contact'} action={'/success'}>
+      <Form
+        className="mt-8 w-full lg:w-9/10 xl:w-5/6"
+        name={'contact'}
+        action={'/success'}
+      >
         <Input
           type={'text'}
           name={'name'}
@@ -59,6 +60,7 @@ class ContactForm extends React.Component {
           autocomplete={true}
           onChange={this.handleInputChange}
         />
+
         <Input
           type={'email'}
           name={'email'}
@@ -67,22 +69,25 @@ class ContactForm extends React.Component {
           autocomplete={true}
           onChange={this.handleInputChange}
         />
+
         <Input
           name={'subject'}
           placeholder={'Subject'}
           value={this.state.subject}
           onChange={this.handleInputChange}
         />
+
         <TextArea
           name={'message'}
           placeholder={'Message'}
           value={this.state.message}
           onChange={this.handleInputChange}
         />
+
         <Button value={'Submit'} />
       </Form>
-    )
+    );
   }
 }
 
-export default ContactPage
+export default ContactPage;
